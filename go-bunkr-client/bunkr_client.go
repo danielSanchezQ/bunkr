ed4fb7bf4d2291cb9e5d480676a68d96ade9524d
danielsanchezq <danielsanchez@off-the-grid.io>
2019-10-23 17:52:57 +0200

    Added bunkr_client documentation to operations results Updated bunkr client (go/python) READMEs Fixed typo on signin operation tag

--- go-bunkr-client/bunkr_client.go
+++ go-bunkr-client/bunkr_client.go
@@ -44,7 +44,7 @@ const (
 	SECRET_INFO                    = "secret-info"
 	SIGN_ECDSA                     = "sign-ecdsa"
 	SSH_PUBLIC_DATA                = "ssh-public-data"
-	SIGNIN                         = "sigin"
+	SIGNIN                         = "signin"
 	CONFIRM_SIGNIN                 = "confirm-signin"
 )
 
@@ -108,34 +108,90 @@ func (bc *BunkrRPCClient) execCmd(cmdName CommandName, args ...string) (Operatio
 	return res.Result, nil
 }
 
+// Returns a JSON like object map[string]interface
+// {
+// 		"msg" : "<feedback message>",
+// }
 func (bc *BunkrRPCClient) NewTextSecret(secretName, content string) (OperationResult, error) {
 	return bc.execCmd(NEW_TEXT_SECRET, secretName, content)
 }
 
+// Returns a JSON like object map[string]interface
+// {
+// 		"msg" : "<feedback message>",
+// }
 func (bc *BunkrRPCClient) NewSSHKey(secretName string) (OperationResult, error) {
 	return bc.execCmd(NEW_SSH_KEY, secretName)
 }
 
+// Returns a JSON like object map[string]interface
+// {
+// 		"msg" : "<feedback message>",
+// }
 func (bc *BunkrRPCClient) NewFileSecret(secretName, filePath string) (OperationResult, error) {
 	return bc.execCmd(NEW_FILE_SECRET, secretName, filePath)
 }
 
+// Returns a JSON like object map[string]interface
+// {
+// 		"msg" : "<feedback message>",
+// }
+func (bc *BunkrRPCClient) NewGroup(groupName string) (OperationResult, error) {
+	return bc.execCmd(NEW_GROUP, groupName)
+}
+
+// Returns a JSON like object map[string]interface
+// {
+// 		"msg" : "<feedback message>",
+// }
 func (bc *BunkrRPCClient) ImportSSHKey(secretName, filePath string) (OperationResult, error) {
 	return bc.execCmd(IMPORT_SSH_KEY, secretName, filePath)
 }
 
+// Returns a JSON like object map[string]interface
+//
+// {
+//		"msg"     : "",
+//		"content" : {
+//			"secrets" : []string, # secrets names
+//			"devices" : {
+//				"<device name>" : []string,
+//				...
+//			},
+//			"groups" : {
+//				"<group name>" : []string,
+//				...
+//			},
+//		}
+// }
 func (bc *BunkrRPCClient) ListSecrets() (OperationResult, error) {
 	return bc.execCmd(LIST_SECRETS)
 }
 
+// Returns a JSON like object map[string]interface
+// {
+// 		"msg" : "",
+//      "devices" : []string
+// }
 func (bc *BunkrRPCClient) ListDevices() (OperationResult, error) {
 	return bc.execCmd(LIST_DEVICES)
 }
 
+// Returns a JSON like object map[string]interface
+// {
+// 		"msg"    : "",
+//      "groups" : []string
+// }
 func (bc *BunkrRPCClient) ListGroups() (OperationResult, error) {
 	return bc.execCmd(LIST_GROUPS)
 }
 
+// Returns a JSON like object map[string]interface
+// {
+// 		"msg"       : "<feedback message>",
+//		"url_raw"   : "<shared static url>",
+//		"url_short" : "<shared short url>",
+// }
 func (bc *BunkrRPCClient) SendDevice(deviceName ...string) (OperationResult, error) {
 	if len(deviceName) > 0 {
 		return bc.execCmd(SEND_DEVICE, deviceName[0])
@@ -143,6 +199,10 @@ func (bc *BunkrRPCClient) SendDevice(deviceName ...string) (OperationResult, err
 	return bc.execCmd(SEND_DEVICE)
 }
 
+// Returns a JSON like object map[string]interface
+// {
+// 		"msg" : "<feedback message>",
+// }
 func (bc *BunkrRPCClient) ReceiveDevice(link string, deviceName ...string) (OperationResult, error) {
 	if len(deviceName) > 0 {
 		return bc.execCmd(RECEIVE_DEVICE, link, deviceName[0])
@@ -150,38 +210,93 @@ func (bc *BunkrRPCClient) ReceiveDevice(link string, deviceName ...string) (Oper
 	return bc.execCmd(RECEIVE_DEVICE, link)
 }
 
+// Returns a JSON like object map[string]interface
+// {
+// 		"msg" : "<feedback message>",
+// }
 func (bc *BunkrRPCClient) RemoveDevice(deviceName string) (OperationResult, error) {
 	return bc.execCmd(REMOVE_DEVICE, deviceName)
 }
 
+// Returns a JSON like object map[string]interface
+// {
+// 		"msg" : "<feedback message>",
+// }
 func (bc *BunkrRPCClient) RemoveLocal(secretName string) (OperationResult, error) {
 	return bc.execCmd(REMOVE_LOCAL, secretName)
 }
 
+// Returns a JSON like object map[string]interface
+// {
+// 		"msg" : "<feedback message>",
+// }
 func (bc *BunkrRPCClient) Rename(oldName, newName string) (OperationResult, error) {
 	return bc.execCmd(RENAME, oldName, newName)
 }
 
+// Returns a JSON like object map[string]interface
+// {
+// 		"msg" : "<feedback message>",
+// }
 func (bc *BunkrRPCClient) Create(secretName, secretType string) (OperationResult, error) {
 	return bc.execCmd(CREATE, secretName, secretType)
 }
 
+// Returns a JSON like object map[string]interface
+// {
+// 		"msg" : "<feedback message>",
+// }
 func (bc *BunkrRPCClient) Write(secretName, contentType, content string) (OperationResult, error) {
 	return bc.execCmd(WRITE, secretName, contentType, content)
 }
 
-func (bc *BunkrRPCClient) Access(secretName string) (OperationResult, error) {
-	return bc.execCmd(ACCESS, secretName)
+// Returns a JSON like object map[string]interface
+// {
+// 		"msg"       : "<feedback message>",
+//		"mode"      : "<access mode>",
+//		"content"   : "<secret content just for (b64 and text)>",
+// }
+func (bc *BunkrRPCClient) Access(secretName string, args ...string) (OperationResult, error) {
+	var mode string
+	var filePath string
+	switch len(args) {
+	case 1:
+		if args[0] != "file" {
+			mode = args[0]
+		}
+	case 2:
+		mode = args[0]
+		filePath = args[1]
+	default:
+	}
+	if filePath != "" {
+		return bc.execCmd(ACCESS, secretName, mode, filePath)
+	}
+	return bc.execCmd(ACCESS, secretName, mode)
 }
 
+// Returns a JSON like object map[string]interface
+// {
+// 		"msg"       : "<feedback message>",
+//		"url_raw"   : "<shared static url>",
+//		"url_short" : "<shared short url>",
+// }
 func (bc *BunkrRPCClient) Grant(targetName, secretName string) (OperationResult, error) {
 	return bc.execCmd(GRANT, targetName, secretName)
 }
 
+// Returns a JSON like object map[string]interface
+// {
+// 		"msg" : "<feedback message>",
+// }
 func (bc *BunkrRPCClient) Revoke(targetName, secretName string) (OperationResult, error) {
 	return bc.execCmd(REVOKE, targetName, secretName)
 }
 
+// Returns a JSON like object map[string]interface
+// {
+// 		"msg" : "<feedback message>",
+// }
 func (bc *BunkrRPCClient) ReceiveCapability(link string, secretName ...string) (OperationResult, error) {
 	if len(secretName) > 0 {
 		return bc.execCmd(RECEIVE_CAPABILITY, link, secretName[0])
@@ -189,38 +304,72 @@ func (bc *BunkrRPCClient) ReceiveCapability(link string, secretName ...string) (
 	return bc.execCmd(RECEIVE_CAPABILITY, link)
 }
 
+// Returns a JSON like object map[string]interface
+// {
+// 		"msg" : "<feedback message>",
+// }
 func (bc *BunkrRPCClient) Delete(secretName string) (OperationResult, error) {
 	return bc.execCmd(DELETE, secretName)
 }
 
+// Returns a JSON like object map[string]interface
+// {
+// 		"msg" : "<feedback message>",
+// }
 func (bc *BunkrRPCClient) ResetTriples(secretName string) (OperationResult, error) {
 	return bc.execCmd(RESET_TRIPLES, secretName)
 }
 
+// Returns a JSON like object map[string]interface
+// {
+// 		"msg" : "<feedback message>",
+// }
 func (bc *BunkrRPCClient) NoOp(secretName string) (OperationResult, error) {
 	return bc.execCmd(NOOP, secretName)
 }
 
+// Returns a JSON like object map[string]interface
+// {
+// 		"msg" : "<feedback message>",
+// }
 func (bc *BunkrRPCClient) SecretInfo(secretName string) (OperationResult, error) {
 	return bc.execCmd(SECRET_INFO, secretName)
 }
 
+// Returns a JSON like object map[string]interface
+// {
+// 		"msg" : "<feedback message>",
+//		"r"   : "<R component of the signature>",
+//		"s"   : "<S component of the signature>",
+// }
 func (bc *BunkrRPCClient) SignECDSA(secretName, hash string) (OperationResult, error) {
 	return bc.execCmd(SIGN_ECDSA, secretName, hash)
 }
 
-func (bc *BunkrRPCClient) NewGroup(groupName string) (OperationResult, error) {
-	return bc.execCmd(NEW_GROUP, groupName)
-}
-
+// Returns a JSON like object map[string]interface
+// {
+// 		"msg"           : "<feedback message>",
+//		"public_data"   : {
+//		    "name"       : "<secret name>",
+//          "public_key" : "<b64 encoded public key>",
+//      }
+// }
 func (bc *BunkrRPCClient) SSHPublicData(secretName string) (OperationResult, error) {
 	return bc.execCmd(SSH_PUBLIC_DATA, secretName)
 }
 
+// Returns a JSON like object map[string]interface
+// {
+// 		"msg" : "<feedback message>",
+// }
 func (bc *BunkrRPCClient) SignIn(email, deviceName string) (OperationResult, error) {
 	return bc.execCmd(SIGNIN, email, deviceName)
 }
 
+// Returns a JSON like object map[string]interface
+// {
+// 		"msg" : "<feedback message>",
+// }
 func (bc *BunkrRPCClient) ConfirmSignIn(email, code string) (OperationResult, error) {
 	return bc.execCmd(CONFIRM_SIGNIN, email, code)
 }
--- py-bunkr-client/README.md
+++ py-bunkr-client/README.md
@@ -13,35 +13,50 @@ Install punkr with pip:
 #### Punkr class
 
 `Punkr` class is the main structure to use. It can work either synchronously or asynchronously. All methods have their async replica, they can be identified by the `async_*` prefix in the method names.
-
-The available commands are:
-
-* create
-* write
-* access
-* delete
-* new-text-secret
-* sign-ecdsa
-
+* new-text-secret       -> create a new secret whose content is a simple text
+* new-ssh-key           -> create a new secret whose content is an ECDSA ssh key
+* new-file-secret       -> dump a file content as a new secret
+* new-group             -> create a new group
+* import-ssh-key        -> import an ssh key from a file into a secret
+* list-secrets          -> list all stored secrets
+* list-devices          -> list all shared devices
+* list-groups           -> list all tracked groups
+* send-device           -> share the current Bunkr device
+* receive-device        -> import a shared Bunkr device
+* remove-device         -> remove a shared device reference from Bunkr
+* remove-local          -> untrack a secret, it does not delete the secret from the plattform
+* rename                -> rename a secret, device or group
+* create                -> create a new empty secret
+* write                 -> write a secret with new content
+* access                -> retrieve the content of a secret
+* grant                 -> grant capabilities to a group or device for an specified secret or group
+* revoke                -> revoke a given capability
+* delete                -> erase a secret existence
+* receive-capability    -> import a capability for a given secret
+* reset-triples         -> synchronize triples for a secret
+* noop-test             -> health check operation over a secret
+* secret-info           -> get secret public information
+* sign-ecdsa            -> make an ECDSA signature with a ECDSA Bunkr secret
+* ssh-public-data       -> retrieve a secret public data
+* signin                -> signin into the platfform
+* confirm-signin        -> confirm the signin process
 
 ## Examples
 
 ```python
 if __name__ == "__main__":
     import asyncio
-    from punkr import Punkr, PunkrException
-    
     # create a connection to the local Bunkr RPC server
-    punkr = Punkr("127.0.0.1", 7860)
+    punkr = Punkr("/tmp/bunkr_daemon.sock")
     try:
         # create a new text secret (synchronously)
-        print(punkr.new_text_secret("MySuperSecret", "secret created from punkr"))
+        print(punkr.new_text_secret("MySuperSecret", 'secret created from punkr'))
         commands = (
-            ("access", {"secret_name": "MySuperSecret"}), # This is the structure of a batch command argument
-            ("access", {"secret_name": "MySuperSecret"}),
-            ("access", {"secret_name": "MySuperSecret"}),
+            ("access", ["MySuperSecret"]), # This is the structure of a batch command argument
+            ("access", ["MySuperSecret"]),
+            ("access", ["MySuperSecret"]),
         )
-        # create corutine to access the secret (asynchronously, order of results is not guaranteed) 
+        # create corutine to access the secret (asynchronously, order of results is not guaranteed)
         async def async_test():
             async for result in punkr.async_batch_commands(*commands):
                 print(result)
@@ -51,7 +66,7 @@ if __name__ == "__main__":
         results1 = asyncio.run(punkr.async_ordered_batch_commands(*commands))
         print(results1)
         # execute a synchronous batch, ordered of execution and results ir guaranteed
-        results2 = punkr.batch_commands(*commands)
+        results2 = list(punkr.batch_commands(*commands))
         print(results2)
         assert results1 == results2
     except PunkrException as e:
